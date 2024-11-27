4e79d20cf4e0bd3570ceef25b8322c795aaad2e7
Andrew-Starson <alberto.candel24@gmail.com>
2024-11-27 18:31:48 -0500

    Add files via upload

---
+++ Final Project/script/main.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+}
+
+const allHeader = document.querySelector('#header-section');
+const allFooter = document.querySelector('#footer-section');
+
+allHeader.innerHTML = `
+    <div class="compname">
+            <h1>Company name</h1>
+            <p>Best retro game seller in Canada</p>
+        </div>
+        <div class="container">
+            <nav>
+                <div class="menu-icon" onclick="toggleMenu()">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                </div>
+                <ul class="nav-links" id="nav-links">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="pages/about.html">About Us</a></li>
+                    <li><a href="pages/contact.html">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+`;
+
+allFooter.innerHTML = `
+    <p id="copy">&copy; Copyright</p>
+`;
