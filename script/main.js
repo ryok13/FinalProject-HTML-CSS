5b9a69bf34e14403d0dc922150e2a987d22656d8
ryok13 <mario24ryok@gmail.com>
2025-07-27 16:41:23 -0400

    Fix dynamic path handling for GitHub Pages compatibility

--- script/main.js
+++ script/main.js
@@ -6,11 +6,19 @@ function toggleMenu() {
 const allHeader = document.querySelector('#header-section');
 const allFooter = document.querySelector('#footer-section');
 
+const isGithubPages = window.location.hostname.includes("github.io");
+const repoName = "FinalProject-HTML-CSS";
+
+// GitHub Pages では `/FinalProject-HTML-CSS/` がルート、ローカルでは `/`
+const base = isGithubPages ? `/${repoName}/` : `/`;
+
+// ページの階層が深い場合に備えて、前に `../` をつけるかどうか
+const prefix = window.location.pathname.includes("/pages/") ? "../" : "";
 
 allHeader.innerHTML = `
     <div class="compname">
 
-            <div class="logo-img"><img src="../images/Georgian1.png" alt="Georgian Retro Games"></div>
+            <div class="logo-img"><img src="${prefix}images/Georgian1.png" alt="Georgian Retro Games"></div>
             <p>Best retro game seller in Canada</p>
         </div>
         <div class="container">
@@ -21,9 +29,9 @@ allHeader.innerHTML = `
                     <div class="bar"></div>
                 </div>
                 <ul class="nav-links" id="nav-links">
-                    <li><a href="../index.html">Home</a></li>
-                    <li><a href="../pages/about.html">About Us</a></li>
-                    <li><a href="../pages/contact.html">Contact Us</a></li>
+                    <li><a href="${base}index.html">Home</a></li>
+                    <li><a href="${base}pages/about.html">About Us</a></li>
+                    <li><a href="${base}pages/contact.html">Contact Us</a></li>
                 </ul>
             </nav>
         </div>
@@ -33,11 +41,11 @@ allFooter.innerHTML = `
   <div class="footer-left">
     <h3>Georgian Retro Games</h3>
     <p class="footer-links">
-      <a href="../index.html" class="link-1">Home</a>
+      <a href="${base}index.html" class="link-1">Home</a>
       |
-      <a href="../pages/about.html">About</a>
+      <a href="${base}pages/about.html">About</a>
       |
-      <a href="../pages/contact.html">Contact</a>
+      <a href="${base}pages/contact.html">Contact</a>
     </p>
     <p class="footer-company-name">© 2024 Georgian Retro Games</p>
   </div>
@@ -59,9 +67,9 @@ allFooter.innerHTML = `
 
   <div class="footer-right">
     <div class="footer-icons">
-      <a href="#"><div class="social"><img src="../images/s1.svg" alt=""></div></a>
-        <a href="#"><div class="social"><img src="../images/s2.svg" alt=""></div></a>
-        <a href="#"><div class="social"><img src="../images/s3.svg" alt=""></div></a>
+      <a href="#"><div class="social"><img src="${prefix}images/s1.svg" alt=""></div></a>
+        <a href="#"><div class="social"><img src="${prefix}images/s2.svg" alt=""></div></a>
+        <a href="#"><div class="social"><img src="${prefix}images/s3.svg" alt=""></div></a>
     </div>
   </div>
 `;
